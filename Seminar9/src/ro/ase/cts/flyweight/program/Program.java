d4473d88a4bdfc4d3ffb8d19cf50d7a8a0a37d00
Georgianaboghiu <boghiugeorgiana7@gmail.comm>
2021-04-22 10:10:35 +0300

    commit flyweight

---
+++ Seminar9/src/ro/ase/cts/flyweight/program/Program.java
@@ -0,0 +1,21 @@
+package ro.ase.cts.flyweight.program;
+
+import ro.ase.cts.flyweight.clase.FlyweightFactory;
+import ro.ase.cts.flyweight.clase.Rezervare;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Rezervare rezervare1=new Rezervare(1,4,"10:00");
+		Rezervare rezervare2=new Rezervare(2,3,"11:00");
+		Rezervare rezervare3=new Rezervare(3,2,"12:00");
+		
+		FlyweightFactory flyweightFactory=new FlyweightFactory();
+		flyweightFactory.getClient("0712345").printeazaRezervare(rezervare1);
+		flyweightFactory.getClient("0752345").printeazaRezervare(rezervare2);
+		flyweightFactory.getClient("0712345").printeazaRezervare(rezervare3);
+	}
+
+}
